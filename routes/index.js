e19895d04131c21806e6b318a729f84d7764d4f0
Hong Zhang <hongzhang0530@icloud.com>
2024-04-05 02:30:32 +0200

    Implemented RESTful API routes for player management

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const {get,getscore,login,upload} = require('../db/dbInterface');
+
+// Route to get all players
+router.get('/getplayer', async (req, res) => {
+  res.send(await get());
+});
+
+// Route to get top 10 scores
+router.get('/getscore', async (req, res) => {
+  res.send(await getscore());
+});
+
+// Route for user login
+router.post('/login', async (req, res) => {
+  res.send(await login(req.body.vorname, req.body.nachname,req.body.profil));
+});
+
+// Route to upload player score
+router.put('/upload', async (req, res) => {
+  res.send(await upload(req.body.nachname, req.body.score));
 });
 
 module.exports = router;
